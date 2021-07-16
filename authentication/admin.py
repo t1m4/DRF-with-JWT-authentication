fd6aaa537b905968be446dbb5b89da8807f278fa
t1m4 <50275532+t1m4@users.noreply.github.com>
2021-07-16 11:25:04 +0300

    Add User in adminpanel

--- authentication/admin.py
+++ authentication/admin.py
@@ -1,19 +1,14 @@
 from django.contrib import admin
-
 # Register your models here.
 from django.contrib.auth.admin import UserAdmin
 
-from django.utils.translation import gettext_lazy as _
-
-# from authentication.models import User
-
-# class MyUserAdmin(UserAdmin):
-# # class MyUserAdmin(admin.ModelAdmin):
-#     readonly_fields = ['date_joined',]
-#     UserAdmin.fieldsets[3][1]['fields'] = ('last_login', 'date_joined', 'last_request')
-
-
-# add new field 'last_request` in UserAdmin and register it
 from authentication.models import User
 
-admin.site.register(User)
+
+class MyUserAdmin(UserAdmin):
+    readonly_fields = ['date_joined', ]
+    # add new field 'last_request` in UserAdmin and register it
+    UserAdmin.fieldsets[3][1]['fields'] = ('last_login', 'date_joined', 'last_request')
+
+
+admin.site.register(User, MyUserAdmin)
